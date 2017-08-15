3849344264587aa4105b0bad2f345b3beac14659
Christian Trabold <ctrabold@thoughtworks.com>
2017-08-16 02:51:00 +0700

    lib: Fix thoughworks_go provider

---
+++ lib/stoplight/providers/thoughtworks_go.rb
@@ -0,0 +1,32 @@
+#
+# Stoplight Provider for ThoughtWorks Go CI (https://www.gocd.org/)
+#
+require 'xmlsimple'
+module Stoplight::Providers
+  class ThoughtworksGo < MultiProjectStandard
+    def provider
+      'thoughtworks_go'
+    end
+
+    def builds_path
+      'go/cctray.xml'
+    end
+
+    def projects
+      if @response.nil? || @response.parsed_response.nil? || @response.parsed_response['Projects'].nil?
+        @projects ||= []
+      else
+        @projects ||= [@response.parsed_response['Projects']['Project']].flatten.collect do |project|
+          Stoplight::Project.new({
+            :name => project['name'],
+            :build_url => project['webUrl'],
+            :last_build_id => project['lastBuildLabel'],
+            :last_build_time => project['lastBuildTime'],
+            :last_build_status => status_to_int(project['lastBuildStatus']),
+            :current_status => activity_to_int(project['activity'])
+          })
+        end
+      end
+    end
+  end
+end
